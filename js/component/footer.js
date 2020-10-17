3dd32d574de580dfaab694d33c4161e12c186135
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-10-17 18:51:46 +0530

    refactoring static components & added issue page.

---
+++ js/component/footer.js
@@ -0,0 +1,18 @@
+window.document.write(`
+  <footer>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#694ea1" fill-opacity="1"
+                d="M0,96L48,90.7C96,85,192,75,288,112C384,149,480,235,576,256C672,277,768,235,864,186.7C960,139,1056,85,1152,80C1248,75,1344,117,1392,138.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+        <div class="py-5 bg-main text-center text-white">
+            <!--main content of footer-->
+            <p>
+                Made with <i class="fa fa-heart text-danger px-2"></i> by our
+                <a href="https://github.com/learn-opensource" class="text-white text-decoration-none" target="_blank">
+                    team of contributors <i class=" px-2 fa fa-users text-white"></i>
+                </a>
+            </p>
+        </div>
+    </footer>
+`);
---
+++ js/component/navbar.js
@@ -0,0 +1,18 @@
+window.document.write(`
+    <nav class="navbar navbar-expand-lg static-top shadow bg-white sticky-top">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="/">
+                <img src="./img/logo.png" height="40" width="auto">
+                <span class="ml-3 text-dark">Learn Opensource</span>
+            </a>
+            <div class="d-flex align-items-center">
+                <a href="issues.html" class="text-dark px-3">
+                    Open Issues
+                </a>
+                <a href="https://github.com/learn-opensource" class="text-dark" target="_blank">
+                    <i class="fa fa-github fa-2x px-2"></i>
+                </a>
+            </div>
+        </div>
+    </nav>
+`);
