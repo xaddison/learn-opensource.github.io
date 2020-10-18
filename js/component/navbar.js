7e7dec375da9a0907f23ea70e935256e76a67434
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-10-18 06:10:06 +0530

    updating AOS features

--- js/component/navbar.js
+++ js/component/navbar.js
@@ -1,12 +1,12 @@
 window.document.write(`
-    <nav class="navbar navbar-expand-lg static-top shadow bg-white sticky-top">
+    <nav class="navbar navbar-expand-lg static-top shadow bg-white sticky-top" data-aos="fade-in">
         <div class="container">
             <a class="navbar-brand d-flex align-items-center" href="/">
                 <img src="./img/logo.png" height="40" width="auto">
                 <span class="ml-3 text-dark">Learn Opensource</span>
             </a>
             <div class="d-flex align-items-center">
-                <a href="issues.html" class="text-dark px-3">
+                <a href="issues.html" class="text-dark px-3 d-none d-md-block">
                     Open Issues
                 </a>
                 <a href="https://github.com/learn-opensource" class="text-dark" target="_blank">
