3a4797c74a3c76ae3cf3de1e2ef0469933ad86bf
Sachin Duhan <38427564+sachin-duhan26@users.noreply.github.com>
2020-10-23 23:14:03 +0530

    Revert "Done with api integration of issue page"

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 import { get_organization_contributors } from "./controller/contributors.js";
 import { get_organization_repos } from "./controller/repos.js";
-import { getRepoIssues } from "./controller/getReposIssues.js";
 
-const pathName = window.location.pathname;
-
-if (pathName === "/issues.html") {
-  getRepoIssues();
-} else if (pathName === "/") {
-  get_organization_repos();
-  get_organization_contributors();
-}
+get_organization_repos();
+get_organization_contributors();
